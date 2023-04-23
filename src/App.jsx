02830a1adb79a9f1f2865ea910c875d6472c1c49
axel <axel.valentinbvs@yahoo.com>
2023-04-23 15:11:36 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
-// testing pages
-import Clients from "./tests/Clients";
-import RetrieveAllDataFromOneID from "./tests/RetrieveAllDataFromOneID";
-import LoginTest from "./tests/LoginTest";
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/clients" element={<Clients />} />
-        <Route path="/retrieve" element={<RetrieveAllDataFromOneID />} />
-        <Route path="/loginTest" element={<LoginTest />} />
       </Routes>
     </BrowserRouter>
   );
