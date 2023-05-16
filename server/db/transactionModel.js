78acd5858da21c659bfc15c690d948612d97943f
Axel <axel.valentinbvs@yahoo.com>
2023-05-16 01:18:31 +0000

    model mongoose for transaction

--- server/db/transactionModel.js
+++ server/db/transactionModel.js
@@ -2,41 +2,45 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const transactionSchema = new Schema({
+  sourceAccount: {
+    type: String, // Compte Number of the source account
+    required: true,
+  },
+  destinationAccount: {
+    type: String, // Compte Number of the destination account
+    required: true,
+  },
   amount: {
     type: Number,
     required: true,
   },
+  currency: {
+    type: String,
+    required: true,
+  },
   description: {
     type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    enum: ["income", "expense"],
-    required: true,
-  },
-  senderId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  recipientId: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+    default: "",
   },
   date: {
     type: Date,
     default: Date.now,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["Dépôt", "Retrait", "Virement"],
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["completed", "pending", "cancelled"],
+    default: "pending",
   },
 });
 
 module.exports = mongoose.model(
-  "Transaction",
+  "Transactions",
   transactionSchema,
-  "transaction"
+  "transactions"
 );
