64f9a66b3b9033c06d44b59868c2d4526639a723
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-05-18 23:30:43 +0000

    remove Link import

--- src/components/ViewCartes.jsx
+++ src/components/ViewCartes.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import logo from "../assets/Logo_white_bg_gray.png";
 import plus from "../assets/plus.png";
 
