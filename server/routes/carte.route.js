4f90569bb9fd7dd8ccd6ffe8c4f4cc62e29418dc
Colas Renard <57412923+cocofox1902@users.noreply.github.com>
2023-06-19 22:48:57 +0200

    Update carte.route.js

--- server/routes/carte.route.js
+++ server/routes/carte.route.js
@@ -64,8 +64,8 @@ Router.get("/carte", async (req, res) => {
 
       if (!updatedUser) {
         return res.status(404).json({ message: "Carte non trouvée" });
+        return res.json(updatedUser);
       }
-      return res.json(updatedUser);
     } catch (err) {
       console.error(err);
       res.status(500).json({ message: "Erreur serveur" });
