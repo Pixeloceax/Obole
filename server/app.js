dd56b29350d26cbd0a0785ed9020c3854666c155
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-06-23 12:47:38 +0000

    allow access origin

--- server/app.js
+++ server/app.js
@@ -22,7 +22,6 @@ dbConnect();
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://obole.vercel.app");
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
