5a56a214d85c61ee156d0c8ac19c81b8bf0f8605
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-08-22 13:35:32 +0000

    add transac

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -2,116 +2,198 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const TransactionForm = () => {
-  const [formData, setFormData] = useState({
-    sourceAccount: "",
-    destinationAccount: "",
-    amount: 0,
-    currency: "",
-    description: "",
-    type: "",
-  });
+  const [amount, setAmount] = useState(0);
+  const [currency, setCurrency] = useState("USD");
+  const [description, setDescription] = useState("");
+  const [transactionType, setTransactionType] = useState("Transfer");
+  const [destinationAccount, setDestinationAccount] = useState("");
+  const [destinationAccountType, setDestinationAccountType] = useState("");
+  const [transactionResponse, setTransactionResponse] = useState(null);
+  const [formFromOption, setFormFromOption] = useState(null);
+  const [formToOption, setFormToOption] = useState(null);
+  const [showSavingAcc, setShowSavingAcc] = useState(false);
+  const [showMainAcc, setShowMainAcc] = useState(false);
+  const [showAccountChoices, setShowAccountChoices] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const transactionFromSavingAccToMainAcc = async (
+    amount,
+    destinationAccountType
+  ) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/transaction/unsaving`,
+        {
+          amount,
+          destinationAccountType,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (response.status === 201) {
+        setTransactionResponse("Transaction Successful");
+      } else {
+        setTransactionResponse("Transaction Failed");
+      }
+    } catch (error) {
+      setTransactionResponse("Transaction Failed");
+    }
+  };
+
+  const transactionFromMainAccToSavingAcc = async (
+    amount,
+    destinationAccountType
+  ) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/transaction/saving`,
+        {
+          amount,
+          destinationAccountType,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (response.status === 201) {
+        setTransactionResponse("Transaction Successful");
+      } else {
+        setTransactionResponse("Transaction Failed");
+      }
+    } catch (error) {
+      setTransactionResponse("Transaction Failed");
+    }
+  };
+
+  const transactionFromMainAccToAcc = async (
+    amount,
+    currency,
+    description,
+    type,
+    destinationAccount
+  ) => {
+    try {
+      const response = await axios.post(
+        `http://localhost:5000/transaction/${parseInt(destinationAccount)}`,
+        {
+          amount,
+          currency,
+          description,
+          type,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (response.status === 201) {
+        setTransactionResponse("Transaction Successful");
+      } else {
+        setTransactionResponse("Transaction Failed");
+      }
+    } catch (error) {
+      setTransactionResponse("Transaction Failed");
+    }
+  };
+
+  const handleFromButtonClick = () => {
+    setShowAccountChoices(true);
+  };
+
+  const handleAccountChoice = (accountType) => {
+    setFormFromOption(accountType);
+    setShowAccountChoices(false);
+    setShowForm(true);
+    if (accountType === "A" || accountType === "jeune") {
+      setShowSavingAcc(true);
+      setShowMainAcc(false);
+      setDestinationAccountType(accountType);
+    } else {
+      setShowMainAcc(true);
+      setShowSavingAcc(false);
+    }
+  };
+
+  const handleToButtonClick = (accountType) => {
+    setFormToOption(accountType);
+    if (accountType === "Saving") {
+      if (formFromOption === "A" || formFromOption === "jeune") {
+        setDestinationAccountType("A");
+      } else {
+        setDestinationAccountType("B");
+      }
+    } else {
+      setShowMainAcc(true);
+      setShowSavingAcc(false);
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    try {
-      const _id = sessionStorage.getItem("_id"); // Get _id from sessionStorage
-      const response = await axios.post(
-        "https://obole-back.onrender.com/transaction",
-        { ...formData, _id } // Include _id in the request body
+    if (destinationAccount === "unsaving" || destinationAccount === "saving") {
+      if (
+        destinationAccountType === "A" ||
+        destinationAccountType === "jeune"
+      ) {
+        transactionFromSavingAccToMainAcc(amount, destinationAccountType);
+      } else {
+        transactionFromMainAccToSavingAcc(amount, destinationAccountType);
+      }
+    } else if (destinationAccount === "account") {
+      transactionFromMainAccToAcc(
+        amount,
+        currency,
+        description,
+        transactionType,
+        destinationAccount
       );
-      console.log(response.data); // Do something with the response
-      // Reset form data
-      setFormData({
-        sourceAccount: "",
-        destinationAccount: "",
-        amount: 0,
-        currency: "",
-        description: "",
-        type: "",
-      });
-    } catch (error) {
-      console.error(error);
     }
   };
 
   return (
-    <div className="bg-white h-screen flex items-center justify-center">
-      <form
-        onSubmit={handleSubmit}
-        className="w-96 bg-[#fff] p-8 shadow-md rounded"
-      >
-        <div className="mb-4">
-          <input
-            type="text"
-            name="sourceAccount"
-            placeholder="Source Account"
-            value={formData.sourceAccount}
-            onChange={handleChange}
-            className="w-full border border-gray-300 px-4 py-2 rounded"
-          />
+    <div>
+      <button onClick={handleFromButtonClick}>
+        {showAccountChoices || showForm ? "Cancel" : "From"}
+      </button>
+      {showAccountChoices && (
+        <div>
+          <button onClick={() => handleAccountChoice("A")}>A</button>
+          <button onClick={() => handleAccountChoice("jeune")}>jeune</button>
         </div>
-        <div className="mb-4">
-          <input
-            type="text"
-            name="destinationAccount"
-            placeholder="Destination Account"
-            value={formData.destinationAccount}
-            onChange={handleChange}
-            className="w-full border border-gray-300 px-4 py-2 rounded"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            type="number"
-            name="amount"
-            placeholder="Amount"
-            value={formData.amount}
-            onChange={handleChange}
-            className="w-full border border-gray-300 px-4 py-2 rounded"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            type="text"
-            name="currency"
-            placeholder="Currency"
-            value={formData.currency}
-            onChange={handleChange}
-            className="w-full border border-gray-300 px-4 py-2 rounded"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            type="text"
-            name="description"
-            placeholder="Description"
-            value={formData.description}
-            onChange={handleChange}
-            className="w-full border border-gray-300 px-4 py-2 rounded"
-          />
-        </div>
-        <div className="mb-4">
-          <input
-            type="text"
-            name="type"
-            placeholder="Type"
-            value={formData.type}
-            onChange={handleChange}
-            className="w-full border border-gray-300 px-4 py-2 rounded"
-          />
-        </div>
-        <button
-          type="submit"
-          className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
-        >
-          Submit
-        </button>
-      </form>
+      )}
+      {showForm && showSavingAcc && (
+        <form onSubmit={handleSubmit}>
+          {formFromOption === "A" && (
+            <>
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Amount"
+              />
+              <input type="hidden" value="A" onChange={() => {}} />
+            </>
+          )}
+          {formFromOption === "jeune" && (
+            <>
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                placeholder="Amount"
+              />
+              <input type="hidden" value="jeune" onChange={() => {}} />
+            </>
+          )}
+          <button type="submit">Submit</button>
+        </form>
+      )}
     </div>
   );
 };
