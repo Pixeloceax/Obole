dbc711db4fb22c02e3851bd48caa1e1e05a7670b
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-08-28 11:41:02 +0000

    generate random jwt secret key and appd to env

---
+++ backend/server/src/utils/jwtKeyGenerator.utils.ts
@@ -0,0 +1,30 @@
+import crypto from "crypto";
+import fs from "fs";
+import path from "path";
+
+function generateJWTKey(): string {
+  const key = crypto.randomBytes(64).toString("hex");
+  return key;
+}
+
+function writeJWTKeyToFile(key: string): void {
+  const envFilePath = path.join(".env");
+  let envContent = "";
+  if (fs.existsSync(envFilePath)) {
+    envContent = fs.readFileSync(envFilePath, "utf-8");
+  }
+  if (!envContent.includes("JWT_KEY=")) {
+    envContent += `JWT_KEY="${key}"\n`;
+    fs.writeFileSync(envFilePath, envContent, { flag: "w" });
+  }
+}
+
+function generateAndWriteJWTKey(): void {
+  const jwtKey = generateJWTKey();
+  writeJWTKeyToFile(jwtKey);
+  console.log("Nouvelle clé JWT générée et écrite dans le fichier .env");
+}
+
+generateAndWriteJWTKey();
+
+export { generateAndWriteJWTKey };
