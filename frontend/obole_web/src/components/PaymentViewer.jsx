c6304194a2ff9d13bb38f0f2eab324f6d6baa9f1
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-09-04 12:22:52 +0000

    add formated date into the table

--- frontend/obole_web/src/components/PaymentViewer.jsx
+++ frontend/obole_web/src/components/PaymentViewer.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect, useState } from "react";
-
-// Import dependencies
 import axios from "axios";
-
-
-// Import assets
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowTrendDown,
-} from "@fortawesome/free-solid-svg-icons";
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const PaymentViewer = () => {
   const [payment, setPayment] = useState([]);
@@ -24,7 +17,12 @@ const PaymentViewer = () => {
             },
           }
         );
-        setPayment(response.data);
+
+        const sortedPayments = response.data.sort((a, b) => {
+          return new Date(b.date) - new Date(a.date);
+        });
+
+        setPayment(sortedPayments);
       } catch (error) {
         throw new Error(error);
       }
@@ -33,6 +31,14 @@ const PaymentViewer = () => {
     fetchData();
   }, []);
 
+  function formatDate(inputDate) {
+    const date = new Date(inputDate);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${year}/${month}/${day}`;
+  }
+
   return (
     <div className="payment-container max-h-[400px] overflow-y-scroll text-white">
       <table className="w-full border">
@@ -40,19 +46,24 @@ const PaymentViewer = () => {
           <tr>
             <th className="p-2 border">Montant</th>
             <th className="p-2 border">Catégorie</th>
+            <th className="p-2 border">Date</th>
           </tr>
         </thead>
         <tbody>
-          {payment.map((singlePayment) => {
-            return (
-              <tr key={singlePayment.id} className="hover:bg-gray-100">
-                <td className="p-2 border">
-                <FontAwesomeIcon icon={faArrowTrendDown} className="text-sm text-red-600 pr-2" />
-                 {singlePayment.amount} €</td>
-                <td className="p-2 border">{singlePayment.categorie}</td>
-              </tr>
-            );
-          })}
+          {payment.map((singlePayment) => (
+            <tr key={singlePayment.id} className="hover:bg-gray-100">
+              <td className="p-2 border">
+                <FontAwesomeIcon
+                  icon={faMinus}
+                  className="text-sm text-red-600 pr-2"
+                />
+                {singlePayment.amount} €
+              </td>
+
+              <td className="p-2 border">{singlePayment.categorie}</td>
+              <td className="p-2 border">{formatDate(singlePayment.date)}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
--- frontend/obole_web/src/components/TransactionViewer.jsx
+++ frontend/obole_web/src/components/TransactionViewer.jsx
@@ -1,14 +1,7 @@
 import React, { useState, useEffect } from "react";
-
-// Import dependencies
 import axios from "axios";
-
-// Import assets
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowTrendUp,
-  faArrowTrendDown,
-} from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const TransactionViewer = () => {
   const [transactions, setTransactions] = useState([]);
@@ -24,7 +17,12 @@ const TransactionViewer = () => {
             },
           }
         );
-        setTransactions(response.data);
+
+        const sortedTransactions = response.data.sort((a, b) => {
+          return new Date(b.date) - new Date(a.date);
+        });
+
+        setTransactions(sortedTransactions);
       } catch (error) {
         throw new Error(error);
       }
@@ -32,6 +30,14 @@ const TransactionViewer = () => {
 
     fetchData();
   }, []);
+
+  function formatDate(inputDate) {
+    const date = new Date(inputDate);
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${year}/${month}/${day}`;
+  }
 
   return (
     <div className="transaction-container max-h-[400px] overflow-y-scroll">
@@ -41,7 +47,8 @@ const TransactionViewer = () => {
             <th className="p-2 border">Montant</th>
             <th className="p-2 border">Description</th>
             <th className="p-2 border">Compte source</th>
-            <th className="p-2 border">Compte de destination</th>
+            <th className="p-2 border">Destinataire</th>
+            <th className="p-2 border">Date</th>
           </tr>
         </thead>
         <tbody>
@@ -50,20 +57,34 @@ const TransactionViewer = () => {
               return (
                 <tr key={transaction.id} className="hover:bg-gray-100">
                   <td className="p-2 border">
-                  <FontAwesomeIcon icon={faArrowTrendUp} className="text-sm text-green-600 pr-2" /> {transaction.amount} €</td>
+                    <FontAwesomeIcon
+                      icon={faPlus}
+                      className="text-sm text-green-600 pr-2"
+                    />{" "}
+                    {transaction.amount} €
+                  </td>
                   <td className="p-2 border">{transaction.description}</td>
                   <td className="p-2 border">{transaction.sourceAccount}</td>
-                  <td className="p-2 border">{transaction.destinationAccount}</td>
+                  <td className="p-2 border">
+                    {transaction.destinationAccount}
+                  </td>
+                  <td className="p-2 border">{formatDate(transaction.date)}</td>
                 </tr>
               );
             }
             return (
               <tr key={transaction.id} className="hover:bg-gray-100">
                 <td className="p-2 border">
-                <FontAwesomeIcon icon={faArrowTrendDown} className="text-sm text-red-600 pr-2" /> {transaction.amount} €</td>
+                  <FontAwesomeIcon
+                    icon={faMinus}
+                    className="text-sm text-red-600 pr-2"
+                  />{" "}
+                  {transaction.amount} €
+                </td>
                 <td className="p-2 border">{transaction.description}</td>
                 <td className="p-2 border">{transaction.sourceAccount}</td>
                 <td className="p-2 border">{transaction.destinationAccount}</td>
+                <td className="p-2 border">{formatDate(transaction.date)}</td>
               </tr>
             );
           })}
