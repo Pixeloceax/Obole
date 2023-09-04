8b132b20b188bc17763fe8cc4a0e8a84b66ba3d3
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-09-04 11:25:48 +0000

    refactor register to send correct information in the back

--- frontend/obole_web/src/pages/Register.jsx
+++ frontend/obole_web/src/pages/Register.jsx
@@ -1,42 +1,59 @@
-import React, { useState, useEffect } from "react";
-
-// Import dependencies
+import React, { useState } from "react";
 import axios from "axios";
-
-// Import assets
 import logo from "../assets/Logo_white_bg_gray.png";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const Register = () => {
   const [message, setMessage] = useState("");
+  const [birthdate, setBirthdate] = useState(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    address: "",
+    phone: "",
+    gender: "",
+    day: "",
+    month: "",
+    year: "",
+  });
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const formDataJSON = Object.fromEntries(formData.entries());
+    try {
+      const dataToSend = {
+        ...formData,
+        day: birthdate.getDate(),
+        month: birthdate.getMonth() + 1,
+        year: birthdate.getFullYear(),
+      };
 
-    const response = await axios.post(
-      "https://obole-back.onrender.com/register",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formDataJSON),
+      const response = await axios.post(
+        `${process.env.REACT_APP_CONNECTION_STRING}/register`,
+        dataToSend
+      );
+
+      const data = response.data;
+      if (data.message === "User Created Successfully") {
+        window.location.href = "/login";
       }
-    );
-
-    const data = await response.json();
-    if (data.message === "User Created Successfully") {
-      window.location.href = "/login";
-    }
-    if (data.message === "Email already exists") {
-      document.querySelector("#email").classList.add("border-red-500");
-      document.querySelector("#alert").classList.remove("hidden");
+      if (data.message === "Email already exists") {
+        setMessage("L'email est déjà utilisé");
+      }
+    } catch (error) {
+      console.error("Error creating user", error);
     }
   };
 
-  useEffect(() => {
-    setMessage("");
-  }, []);
   return (
     <div className="md:min-h-screen flex items-center justify-center bg-white">
       <div className="">
@@ -46,7 +63,7 @@ const Register = () => {
           </div>
           <div>
             <h2 className="text-center text-3xl font-extrabold text-white">
-              Register
+              Inscription
             </h2>
           </div>
           <form
@@ -57,24 +74,28 @@ const Register = () => {
               <div className="justify-center md:p-0 p-5">
                 <div className="mb-5">
                   <input
-                    id="nom"
-                    name="nom"
+                    id="name"
+                    name="name"
                     type="text"
-                    autocomplete="nom"
+                    autoComplete="name"
                     required
                     className="input appearance-none relative block w-full px-3 mr-20 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-full sm:text-sm"
-                    placeholder="Nom"
+                    placeholder="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
                   />
                 </div>
                 <div>
                   <input
-                    id="prenom"
-                    name="prenom"
+                    id="lastName"
+                    name="lastName"
                     type="text"
-                    autocomplete="prenom"
+                    autoComplete="lastName"
                     required
                     className="input appearance-none relative block w-full px-3 mr-20 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-full sm:text-sm"
-                    placeholder="Prenom"
+                    placeholder="lastName"
+                    value={formData.lastName}
+                    onChange={handleInputChange}
                   />
                 </div>
                 <div className="mt-5">
@@ -82,12 +103,28 @@ const Register = () => {
                     id="email"
                     name="email"
                     type="email"
-                    autocomplete="email"
+                    autoComplete="email"
                     required
                     className="input appearance-none relative block w-full px-3 mr-20 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-full sm:text-sm"
                     placeholder="Email"
+                    value={formData.email}
+                    onChange={handleInputChange}
                   />
                 </div>
+
+                <div className="mt-5">
+                  <input
+                    id="country"
+                    name="country"
+                    autoComplete="country"
+                    required
+                    className="input appearance-none relative block w-full px-3 mr-20 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-full sm:text-sm"
+                    placeholder="Pays"
+                    value={formData.country}
+                    onChange={handleInputChange}
+                  ></input>
+                </div>
+
                 <div className="h-2 pt-2">
                   <div id="alert" className="text-red-500 text-center hidden">
                     L'email est deja utiliser
@@ -97,35 +134,54 @@ const Register = () => {
               <div className="justify-center md:p-0 p-5 md:ml-5">
                 <div className="mb-5">
                   <input
-                    id="adresse"
-                    name="adresse"
+                    id="address"
+                    name="address"
                     type="text"
-                    autocomplete="adresse"
+                    autoComplete="address"
                     required
                     className="input appearance-none relative block w-full px-3 mr-20 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-full sm:text-sm"
-                    placeholder="Adresse"
+                    placeholder="address"
+                    value={formData.address}
+                    onChange={handleInputChange}
                   />
                 </div>
-                <div>
+                <div className="mb-5">
                   <input
-                    id="tel"
-                    name="tel"
-                    type="tel"
-                    autocomplete="tel"
+                    id="phone"
+                    name="phone"
+                    type="phone"
+                    autoComplete="phone"
                     required
                     className="input appearance-none relative block w-full px-3 mr-20 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-full sm:text-sm"
                     placeholder="Numéro de téléphone"
+                    value={formData.phone}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div>
+                  <DatePicker
+                    id="birthdate"
+                    name="birthdate"
+                    selected={birthdate || null}
+                    onChange={(date) => setBirthdate(date)}
+                    dateFormat="dd/MM/yyyy"
+                    required
+                    className="input appearance-none relative block w-full px-3 mr-20 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-full sm:text-sm"
+                    placeholderText="Sélectionnez une date"
                   />
                 </div>
               </div>
             </div>
+
             <div className="flex items-center text-white justify-center">
               <label className="inline-flex items-center">
                 <input
                   type="radio"
                   className="form-radio text-blue-500"
-                  name="genre"
+                  name="gender"
                   value="male"
+                  checked={formData.gender === "male"}
+                  onChange={handleInputChange}
                 />
                 <span className="ml-2">Homme</span>
               </label>
@@ -133,8 +189,10 @@ const Register = () => {
                 <input
                   type="radio"
                   className="form-radio text-pink-500"
-                  name="genre"
+                  name="gender"
                   value="female"
+                  checked={formData.gender === "female"}
+                  onChange={handleInputChange}
                 />
                 <span className="ml-2">Femme</span>
               </label>
@@ -144,7 +202,7 @@ const Register = () => {
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Register
+                Inscription
               </button>
             </div>
           </form>
